75d2f0437e60e6ac9773b7ab5ed6c4464bbdeb44
CarolPini44 <carolpini10@gmail.com>
2025-10-24 11:08:57 -0300

    ex1

--- dev1Carolina.py
+++ dev1Carolina.py
@@ -1,1 +1,22 @@
-print('bunda)
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    lista_posicoes = []
+    grid = [
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],]
+    
+    if orientacao == 'vertical':
+        for i in range(tamanho):
+            lista_posicoes.append([linha + i, coluna])
+    elif orientacao == 'horizontal':
+        for i in range(tamanho):
+            lista_posicoes.append([linha, coluna + i])
+    
+    return lista_posicoes
