3277df55c3c6e2f81111ddb964c0e6183d32df15
tigabriel09-coder <ti.gabriel09@gmail.com>
2025-10-24 19:07:09 -0300

    ex 6

--- funcoes.py
+++ funcoes.py
@@ -67,3 +67,36 @@ def afundados(frota, tabuleiro):
                 total_afundados += 1
 
     return total_afundados
+
+
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    """
+    Retorna a lista de posições (cada uma como [linha, coluna]) ocupadas por um navio
+    começando em (linha, coluna) com a orientacao ('horizontal' ou 'vertical') e dado tamanho.
+    """
+    posicoes = []
+    if orientacao == 'horizontal':
+        for c in range(coluna, coluna + tamanho):
+            posicoes.append([linha, c])
+    elif orientacao == 'vertical':
+        for r in range(linha, linha + tamanho):
+            posicoes.append([r, coluna])
+    else:
+        raise ValueError("orientacao deve ser 'horizontal' ou 'vertical'")
+    return posicoes
+
+
+def posicao_valida(frota, linha, coluna, orientacao, tamanho):
+   
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+
+    for r, c in posicoes:
+        if r < 0 or r > 9 or c < 0 or c > 9:
+            return False
+    for lista_navios in frota.values():
+        for navio in lista_navios:
+            for parte in navio:
+                if parte in posicoes:
+                    return False
+
+    return True
