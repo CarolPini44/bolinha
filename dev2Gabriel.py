7a210cb27674949ab517c05e6e69deb78a121d46
tigabriel09-coder <ti.gabriel09@gmail.com>
2025-10-24 15:12:43 -0300

    ex 5

--- dev2Gabriel.py
+++ dev2Gabriel.py
@@ -1,34 +1,12 @@
-def define_posicoes(linha, coluna, orientacao, tamanho):
-    lista_posicoes = []
-    grid = [
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],]
-    
-    if orientacao == 'vertical':
-        for i in range(tamanho):
-            lista_posicoes.append([linha + i, coluna])
-    elif orientacao == 'horizontal':
-        for i in range(tamanho):
-            lista_posicoes.append([linha, coluna + i])
-    
-    return lista_posicoes
+def afundados(frota, tabuleiro):
+    total_afundados = 0
 
-def preenche_frota(frota, nome_do_navio, linha, coluna, orientacao, tamanho):
     
-    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+    for navios in frota.values():
+        for navio in navios:
+            afundado = all(tabuleiro[linha][coluna] == 'X' for linha, coluna in navio)
+            if afundado:
+                total_afundados += 1
 
-    if nome_do_navio not in frota:
-        frota[nome_do_navio] = [posicoes]
-    else:
-        frota[nome_do_navio].append(posicoes)
-        
+    return total_afundados
 
-    return frota
