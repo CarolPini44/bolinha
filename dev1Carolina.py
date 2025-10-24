e5f8fb5df5a8a3fa5e6764a3c6a6c7bb2d79c502
CarolPini44 <carolpini10@gmail.com>
2025-10-24 12:11:32 -0300

    ex1 e ex3

--- dev1Carolina.py
+++ dev1Carolina.py
@@ -21,15 +21,6 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
     
     return lista_posicoes
 
-def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
-    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
-    if nome_navio in frota:
-        frota[nome_navio].append(posicoes)
-    else:
-        frota[nome_navio] = [posicoes]
-
-    return frota
-
 def faz_jogada(tabuleiro, linha, coluna):
     if tabuleiro[linha][coluna] == 1:
         tabuleiro[linha][coluna] = 'X'
