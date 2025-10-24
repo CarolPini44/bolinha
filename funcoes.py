0da1c828221751e633f61deab331498d71e179e5
CarolPini44 <carolpini10@gmail.com>
2025-10-24 10:55:48 -0300

    ex1 aaaaaaaaaa

--- funcoes.py
+++ funcoes.py
@@ -1,6 +1,5 @@
 def define_posicoes(linha, coluna, orientacao, tamanho):
     lista_posicoes = []
-    grid = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
     if orientacao == 'vertical':
         for i in range(tamanho):
             lista_posicoes.append([linha + i, coluna])
