0b74194881f94f1bddcb77a9afd9c849cdb70ebf
CarolPini44 <carolpini10@gmail.com>
2025-10-24 12:23:32 -0300

    ex3

--- funcoes.py
+++ funcoes.py
@@ -1,38 +1,3 @@
-def define_posicoes(linha, coluna, orientacao, tamanho):
-    lista_posicoes = []
-    grid = [
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],]
-    
-    if orientacao == 'vertical':
-        for i in range(tamanho):
-            lista_posicoes.append([linha + i, coluna])
-    elif orientacao == 'horizontal':
-        for i in range(tamanho):
-            lista_posicoes.append([linha, coluna + i])
-    
-    return lista_posicoes
-
-def preenche_frota(frota, nome_do_navio, linha, coluna, orientacao, tamanho):
-    
-    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
-
-    if nome_do_navio not in frota:
-        frota[nome_do_navio] = [posicoes]
-    else:
-        frota[nome_do_navio].append(posicoes)
-        
-
-    return frota
-
 def faz_jogada(tabuleiro, linha, coluna):
     if tabuleiro[linha][coluna] == 1:
         tabuleiro[linha][coluna] = 'X'
