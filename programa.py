bb569d1488feca073eea29f3c1eb4d847c78c326
CarolPini44 <carolpini10@gmail.com>
2025-10-24 20:37:33 -0300

    arranjo

--- programa.py
+++ programa.py
@@ -18,25 +18,20 @@ for nome, tamanho, quantidade in embarcacoes:
     for i in range(quantidade):
         posicionado = False
         while not posicionado:
-            print(f"Insira as informações referentes ao navio {nome} que possui tamanho {tamanho}")
-            linha = int(input("Linha: "))
-            coluna = int(input("Coluna: "))
+            print(f'Insira as informações referentes ao navio {nome} que possui tamanho {tamanho}')
+            linha = int(input('Linha: '))
+            coluna = int(input('Coluna: '))
 
             if tamanho == 1:
-                orientacao = 'horizontal'  
+                orientacao = 'horizontal'
             else:
-                opc = int(input("[1] Vertical [2] Horizontal >"))
+                opc = int(input("1 Vertical e 2 Horizontal >"))
                 orientacao = 'vertical' if opc == 1 else 'horizontal'
 
             if posicao_valida(frota, linha, coluna, orientacao, tamanho):
-                posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
                 preenche_frota(frota, nome, linha, coluna, orientacao, tamanho)
                 posicionado = True
             else:
                 print("Esta posição não está válida!")
 
-    print(frota)
-
-if __name__ == "__main__":
-    main()
-
+print(frota)
