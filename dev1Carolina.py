aeb6d2e8bee5ae91356ee25291ead8ce111bb8f1
CarolPini44 <carolpini10@gmail.com>
2025-10-24 14:11:48 -0300

    ex4

--- dev1Carolina.py
+++ dev1Carolina.py
@@ -1,3 +1,4 @@
+#exercício 1
 def define_posicoes(linha, coluna, orientacao, tamanho):
     lista_posicoes = []
     grid = [
@@ -21,9 +22,27 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
     
     return lista_posicoes
 
+#exercício 3
 def faz_jogada(tabuleiro, linha, coluna):
     if tabuleiro[linha][coluna] == 1:
         tabuleiro[linha][coluna] = 'X'
     else:
         tabuleiro[linha][coluna] = '-'
+    return tabuleiro
+
+#exercício 4
+def posiciona_frota(frota):
+    tabuleiro = []
+    for linha in range(10):
+        linha = []
+        for coluna in range(10):
+            linha.append(0)
+        tabuleiro.append(linha)
+
+    for navio in frota:
+        for qnt_navio in frota[navio]:
+            for pos in qnt_navio:
+                linha = pos[0]
+                coluna = pos[1]
+                tabuleiro[linha][coluna] = 1  
     return tabuleiro
