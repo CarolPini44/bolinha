c9d9e253972c3e739057d5c8d7ea549e6218f718
CarolPini44 <carolpini10@gmail.com>
2025-10-24 11:35:08 -0300

    ex2

--- dev1Carolina.py
+++ dev1Carolina.py
@@ -20,3 +20,15 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
             lista_posicoes.append([linha, coluna + i])
     
     return lista_posicoes
+
+def preenche_frota(frota, nome_do_navio, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+    if orientacao == 'vertical':
+        for i in range(tamanho):
+            frota[nome_do_navio] = [posicoes]
+    if orientacao == 'horizontal':
+        for i in range(tamanho):
+            frota[nome_do_navio] = [posicoes]
+
+    return frota
+
