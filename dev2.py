e7cde8fa7fde04a18da2d28d314da558afa5afac
tigabriel09-coder <ti.gabriel09@gmail.com>
2025-10-24 10:38:30 -0300

    exercicio1

--- dev2.py
+++ dev2.py
@@ -1,1 +1,23 @@
-print("hello world")
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    posicoes = []
+    grid = [
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+  ]
+
+    if orientacao == "vertical":
+        for i in range(tamanho):
+            posicoes.append([linha + i, coluna])
+    elif orientacao == "horizontal":
+        for i in range(tamanho):
+            posicoes.append([linha, coluna + i])
+
+    return posicoes
