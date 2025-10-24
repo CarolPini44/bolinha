9863a86af74aadc29899ffeabad732d57255d6bc
CarolPini44 <carolpini10@gmail.com>
2025-10-24 11:46:59 -0300

    ex2

--- dev1Carolina.py
+++ dev1Carolina.py
@@ -21,14 +21,25 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
     
     return lista_posicoes
 
-def preenche_frota(frota, nome_do_navio, linha, coluna, orientacao, tamanho):
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
     posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
-    if orientacao == 'vertical':
-        for i in range(tamanho):
-            frota[nome_do_navio] = [posicoes]
-    if orientacao == 'horizontal':
-        for i in range(tamanho):
-            frota[nome_do_navio] = [posicoes]
+    if nome_navio in frota:
+        frota[nome_navio].append(posicoes)
+    else:
+        frota[nome_navio] = [posicoes]
 
     return frota
+
+frota = {
+  "navio-tanque":[
+    [[6,1],[6,2],[6,3]]
+  ]
+}
+nome_navio = 'navio-tanque'
+linha = 4
+coluna = 7
+orientacao = 'vertical'
+tamanho = 3
+y = preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho)
+print(y)
 
